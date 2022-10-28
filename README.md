e64fbbc8ae946e79615b18bd161c3f392e00ecd3
Jesulayomy <jesulayomi081@gmail.com>
2022-10-28 21:10:36 +0000

    framework

---
+++ README.md
@@ -0,0 +1,1 @@
+Working on it geez, you're blocking my creative flow everytime you bring it up
---
+++ bank/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+#endif
